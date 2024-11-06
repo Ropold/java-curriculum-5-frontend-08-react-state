1510d2083d0f914d8b739783f88606d42ad53204
Robert Stolz <robert.stolz87@gmail.com>
2024-11-06 16:40:21 +0100

    new character added

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,6 +6,7 @@ export default function Home(){
             <h1>Home - Welcome</h1>
             <Link to={"/characters"}>Characters </Link>
             <Link to={"/planets"}>Planets </Link>
+            <Link to={"/newcharacter"}>New Character </Link>
         </div>
     );
 }
